3b809d47ec649dbeca428e8099adfb17ad5395d7
ivanko144 <ivan.kalyuzhnyy@mail.ru>
2015-05-14 07:14:15 +0100

    1. makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,26 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+# variable x must be square matrix  # x1 <- (matrix(c(1,2,3, 11,12,13, 1,2,1), nrow = 3, ncol = 3, byrow = TRUE))
+#>                                  # x2 <- (matrix(c(1,1,3, 11,12,13, 1,2,2), nrow = 3, ncol = 3, byrow = TRUE))
+#
+# aa <- makeCacheMatrix(x1)   # return special "matrix" object wich consist of four function
+# aa$get()                    # return original matrix (fgrom cache or from memory)
+# aa$set(x2)                  # update (set) cached value of original matrix; ff - new square matrix
+# aa$setinverse()             # cache inversed matrix
+# aa$getinverse()             # get cached  matrix from special "matrix" object; return NULL before first using setinverce()
+
+  m <- NULL
+  set <- function(y) {                       # update (set) cached value of x matrix
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x                        # get x matrix (from cache or from memory)
+  setinverse <- function() m <<- solve(x)    # update cached value of inverced matrix
+  getinverse <- function() m                 # get cached inverced matrix
+  list(set = set, get = get,                 # return special "matrix" object
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
